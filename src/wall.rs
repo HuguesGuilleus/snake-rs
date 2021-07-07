1b9681c3dc0d7efdc6398b425c3d75e823faa4bc
Hugues GUILLEUS <hugues.guilleus@csgroup.eu>
2021-07-07 13:14:48 +0200

    wall.rs: init on wall

--- src/wall.rs
+++ src/wall.rs
@@ -16,14 +16,24 @@ impl Wall {
         }
     }
 
-    fn iter(&self) -> impl Iterator<Item = (usize, usize)> {
-        (self.begin_x..self.end_x).map(|x| (x, 0))
+    pub fn iter(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
+        (self.begin_x..self.end_x)
+            .map(move |x| (self.begin_y..self.end_y).map(move |y| (x, y)))
+            .flatten()
     }
 
     /// Tell if a point if over the wall
     pub fn over(&self, x: usize, y: usize) -> bool {
         self.begin_x <= x && x < self.end_x && self.begin_y <= y && y < self.end_y
     }
+}
+
+#[test]
+fn wall_iter() {
+    let r = Wall::new(3, 5, 1, 2)
+        .iter()
+        .collect::<Vec<(usize, usize)>>();
+    assert_eq!(vec![(3, 1), (4, 1)], r);
 }
 
 #[test]
